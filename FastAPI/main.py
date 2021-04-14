6d38a269c762e806cdfe6d13c86df7402dbffd29
Sadayoshi Tada <tada@snaq.me>
2021-04-14 12:33:22 +0900

    X-RAY組込

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+patch_all()
 
 app = FastAPI()
 
-
+xray_recorder.begin_segment('FastAPITest')
 @app.get("/")
 def read_root():
     return{"Hello":"World"}
+
+xray_recorder.end_segment()
